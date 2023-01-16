3f582a801c3c2701b6565a8abcb6a50f94f1301b
Jose A. Abell <jaabell@uandes.cl>
2023-01-16 13:56:24 -0300

    adding new thermal 10nt

--- SRC/element/TclElementCommands.cpp
+++ SRC/element/TclElementCommands.cpp
@@ -167,6 +167,7 @@ extern void *OPS_ASDEmbeddedNodeElement(void); // Massimo Petracca (ASDEA)
 extern void *OPS_ShellANDeS(void);
 extern void *OPS_FourNodeTetrahedron(void);
 extern void *OPS_TenNodeTetrahedron(void);
+extern void *OPS_TenNodeTetrahedronThermal(void);
 extern void *OPS_LysmerTriangle(void);
 extern void *OPS_ASDAbsorbingBoundary2D(void); // Massimo Petracca (ASDEA)
 extern void *OPS_ASDAbsorbingBoundary3D(void); // Massimo Petracca (ASDEA)
@@ -1349,7 +1350,20 @@ TclModelBuilderElementCommand(ClientData clientData, Tcl_Interp *interp,
       }
 
   }
-  
+  else if (strcmp(argv[1], "TenNodeTetrahedronThermal") == 0) {
+      void *theEle = OPS_TenNodeTetrahedronThermal();
+      if (theEle != 0) 
+      {
+        theElement = (Element*)theEle;
+      } 
+      else 
+      {
+        opserr<<"tclelementcommand -- unable to create element of type : "
+            <<argv[1]<<endln;
+        return TCL_ERROR;
+      }
+
+  }
   else if (strcmp(argv[1],"ZeroLengthVG_HG") == 0) {
       Element *theEle = (Element*) OPS_ZeroLengthVG_HG();
     if (theEle != 0) 
