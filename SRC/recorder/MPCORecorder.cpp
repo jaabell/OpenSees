371a5ed98048becf7e638cde13f31a70eff94e68
Jose Larenas <111939223+j0selarenas@users.noreply.github.com>
2023-07-28 12:06:52 -0400

    swapped nodes 9 & 10 for 10nt & 10nt-thermal in mpco

--- SRC/recorder/MPCORecorder.cpp
+++ SRC/recorder/MPCORecorder.cpp
@@ -5027,8 +5027,19 @@ int MPCORecorder::writeModelElements()
 						it4 != elem_by_custom_rule.items.end(); ++it4) {
 						Element *elem = *it4;
 						buffer[offset] = elem->getTag();
+
+
+						int classTag = elem->getClassTag();
+
 						for (size_t j = 0; j < elem_by_tag.num_nodes; j++)
 							buffer[j + 1 + offset] = elem->getExternalNodes()((int)j);
+
+						// Changes nodes 9 & 10 for 10nt & 10nt-thermal
+						if (classTag == ELE_TAG_TenNodeTetrahedron || classTag == ELE_TAG_TenNodeTetrahedronThermal){
+							int aux = buffer[8 + 1 + offset] ;
+							buffer[8 + 1 + offset] = buffer[9 + 1 + offset] ;
+							buffer[9 + 1 + offset] = aux ;
+						}
 						offset += (1 + elem_by_tag.num_nodes);
 					}
 					hid_t dset_id = h5::dataset::createAndWrite(h_gp_elements, elem_by_custom_rule.name.c_str(), buffer, elem_by_custom_rule.items.size(), (hsize_t)(1 + elem_by_tag.num_nodes));
